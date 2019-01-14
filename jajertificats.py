7334410106424e171199caeb13c3b4cb9cadac69
valoulou <valentin.chaigneau@gmail.com>
2019-01-14 22:37:39 +0100

    Expiration check

--- jajertificats.py
+++ jajertificats.py
@@ -36,7 +36,7 @@ with open(FILE_DOMAINS, "r") as f:
 		currentdomain = currentdomain.replace('\n', '')
 		result = sslExpirationStatus(currentdomain, EXPIRATION_DAYS)
 		if result["status"] == "expired":
-			print '\033[1;31m[!]\033[1;m'+" Certificat pour "+currentdomain+" expiré"
+			print '\033[1;31m[!]\033[1;m'+" Certificat pour "+currentdomain+" expiré depuis "+str(result["date"]*-1)+" jours"
 		elif result["status"] == "expiresoon":
 			print '\033[1;33m[!]\033[1;m'+" Certificat pour "+currentdomain+" expire dans "+str(result["date"])+" jours"
 		else:
