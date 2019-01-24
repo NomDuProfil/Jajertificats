4c4c68c470e035f23625a1c466f88a15d35d9b2f
valoulou <valentin.chaigneau@gmail.com>
2019-01-25 00:03:17 +0100

    updates

--- jajertificats.py
+++ jajertificats.py
@@ -19,31 +19,39 @@ PERIODE_DE_VALIDITE = 825 #En nombre de jour
 
 LIST_PORT = ['443', '8443']
 
-def writeresult(result):
+def writeresult(result, pathsave="./"):
 	wb = Workbook()
 	ws = wb.active
+	listwidthcolumn = [] 
 	ws['A1'] = "Status certificat"
 	ws['A1'].alignment = Alignment(horizontal='center')
 	ws['A1'].font = Font(bold=True)
+	ws.column_dimensions['A'].width = len("Status certificat")*1.3
 	ws['B1'] = "Domaines"
 	ws['B1'].alignment = Alignment(horizontal='center')
 	ws['B1'].font = Font(bold=True)
 	ws['C1'] = "Port"
+	ws.column_dimensions['C'].width = len("Port")*1.3
 	ws['C1'].alignment = Alignment(horizontal='center')
 	ws['C1'].font = Font(bold=True)
 	ws['D1'] = "Date de délivrance"
+	ws.column_dimensions['D'].width = len("Date de délivrance")*1.3
 	ws['D1'].alignment = Alignment(horizontal='center')
 	ws['D1'].font = Font(bold=True)
 	ws['E1'] = "Date d'expiration"
+	ws.column_dimensions['E'].width = len("Date d'expiration")*1.3
 	ws['E1'].alignment = Alignment(horizontal='center')
 	ws['E1'].font = Font(bold=True)
 	ws['F1'] = "Jours restants"
+	ws.column_dimensions['F'].width = len("Jours restants")*1.3
 	ws['F1'].alignment = Alignment(horizontal='center')
 	ws['F1'].font = Font(bold=True)
 	ws['G1'] = "Validité (nombre de jours)"
+	ws.column_dimensions['G'].width = len("Validité (nombre de jours)")*1.3
 	ws['G1'].alignment = Alignment(horizontal='center')
 	ws['G1'].font = Font(bold=True)
 	ws['H1'] = "Vérifié par"
+	ws.column_dimensions['H'].width = len("Vérifié par")*1.3
 	ws['H1'].alignment = Alignment(horizontal='center')
 	ws['H1'].font = Font(bold=True)
 	currentline = 2
@@ -58,35 +66,41 @@ def writeresult(result):
 			ws['A'+str(currentline)] = "ÉXPIRE BIENTOT"
 			ws['A'+str(currentline)].fill = PatternFill(fill_type="solid", start_color='FF9900', end_color='FF9900')
 		elif current["status"] == "validitytoolong":
+			if ws.column_dimensions['A'].width < len("VALIDITE TROP LONGUE")*1.3:
+				ws.column_dimensions['A'].width = len("VALIDITE TROP LONGUE")*1.3
 			ws['A'+str(currentline)] = "VALIDITÉ TROP LONGUE"
 			ws['A'+str(currentline)].fill = PatternFill(fill_type="solid", start_color='FF9900', end_color='FF9900')
+		elif current["status"] == "NOT_MATCH":
+			if ws.column_dimensions['A'].width < len("HOSTNAME INVALIDE")*1.3:
+				ws.column_dimensions['A'].width = len("HOSTNAME INVALIDE")*1.3
+			ws['A'+str(currentline)] = "HOSTNAME INVALIDE"
+			ws['A'+str(currentline)].fill = PatternFill(fill_type="solid", start_color='FF0000', end_color='FF0000')
 		else:
 			ws['A'+str(currentline)] = current["status"]
 			ws['A'+str(currentline)].fill = PatternFill(fill_type="solid", start_color='FF0000', end_color='FF0000')
 		ws['A'+str(currentline)].font = Font(color='FFFFFF')
 		ws['A'+str(currentline)].alignment = Alignment(horizontal='center')
-		ws['B'+str(currentline)] = current["domain"].encode('utf8')
-		ws['B'+str(currentline)].alignment = Alignment(horizontal='center')
+		ws['B'+str(currentline)] = current["domain"]
+		if ws.column_dimensions['B'].width < len(current["domain"])*1.3:
+			ws.column_dimensions['B'].width = len(current["domain"])*1.3
 		ws['C'+str(currentline)] = current["port"]
-		ws['C'+str(currentline)].alignment = Alignment(horizontal='center')
+		if ws.column_dimensions['C'].width < len(current["port"])*1.3:
+			ws.column_dimensions['C'].width = len(current["port"])*1.3
 		if (str(current["notBefore"]) != "ERROR"):
 			ws['D'+str(currentline)] = str(current["notBefore"].year)+'-'+str(current["notBefore"].month)+'-'+str(current["notBefore"].day)
 		else:
 			ws['D'+str(currentline)] = "ERROR"
-		ws['D'+str(currentline)].alignment = Alignment(horizontal='center')
 		if (str(current["notAfter"]) != "ERROR"):
 			ws['E'+str(currentline)] = str(current["notAfter"].year)+'-'+str(current["notAfter"].month)+'-'+str(current["notAfter"].day)
 		else:
 			ws['E'+str(currentline)] = "ERROR"
-		ws['E'+str(currentline)].alignment = Alignment(horizontal='center')
 		ws['F'+str(currentline)] = str(current["deltaToday"])
-		ws['F'+str(currentline)].alignment = Alignment(horizontal='center')
 		ws['G'+str(currentline)] = str(current["periodevalidity"])
-		ws['G'+str(currentline)].alignment = Alignment(horizontal='center')
 		ws['H'+str(currentline)] = current["deliver"]
-		ws['H'+str(currentline)].alignment = Alignment(horizontal='center')
+		if ws.column_dimensions['H'].width < len(current["deliver"])*1.3:
+			ws.column_dimensions['H'].width = len(current["deliver"])*1.3
 		currentline+=1
-	wb.save('StatusCertificates.xlsx')
+	wb.save(pathsave+'StatusCertificates.xlsx')
 
 def sslExpirationDate(address, port):
 	if DUREE_ENTRE_CHAQUE_REQUETE != 0:
@@ -97,17 +111,50 @@ def sslExpirationDate(address, port):
 		certinfo = {}
 		certinfo["notBefore"] = datetime.datetime.strptime(certLoad.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ')
 		certinfo["notAfter"] = datetime.datetime.strptime(certLoad.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ')
-		certinfo["deliver"] = certLoad.get_issuer().CN
+		if certLoad.get_issuer().CN != None:
+			certinfo["deliver"] = certLoad.get_issuer().CN
+		elif certLoad.get_issuer().O != None:
+			certinfo["deliver"] = certLoad.get_issuer().O
+		else:
+			certinfo["deliver"] = "Introuvable"
 		return certinfo
 	except ssl.SSLError as err:
-		if ("WRONG_SSL_VERSION" in str(err)):
+		if ("SSLV3_ALERT" in str(err) or "WRONG_SIGNATURE_TYPE" in str(err) or "TLSV1_ALERT_INTERNAL_ERROR" in str(err)):
+			try:
+				context = ssl.create_default_context()
+				conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=address)
+				conn.connect((address, int(port)))
+				cert = conn.getpeercert()
+				certinfo = {}
+				certinfo["notBefore"] = datetime.datetime.strptime(cert["notBefore"].decode('ascii'), '%b %d %H:%M:%S %Y %Z')
+				certinfo["notAfter"] = datetime.datetime.strptime(cert["notAfter"].decode('ascii'), '%b %d %H:%M:%S %Y %Z')
+				try:
+					certinfo["deliver"] = cert["issuer"][3][0][1]
+				except IndexError:
+					certinfo["deliver"] = cert["issuer"][2][0][1]
+				conn.close()
+				return certinfo
+			except socket.error as err:
+				#print str(err)
+				return "ERROR"
+			except ssl.CertificateError as err:
+				#print str(err)
+				if ("doesn't match either of" in str(err)):
+					return "NOT_MATCH"
+				return "ERROR"
+		elif ("WRONG_SSL_VERSION" in str(err) or "UNSUPPORTED_PROTOCOL" in str(err)):
 			try:
 				cert=ssl.get_server_certificate((address, int(port)), ssl_version=ssl.PROTOCOL_TLSv1)
 				certLoad = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
 				certinfo = {}
 				certinfo["notBefore"] = datetime.datetime.strptime(certLoad.get_notBefore().decode('ascii'), '%Y%m%d%H%M%SZ')
 				certinfo["notAfter"] = datetime.datetime.strptime(certLoad.get_notAfter().decode('ascii'), '%Y%m%d%H%M%SZ')
-				certinfo["deliver"] = certLoad.get_issuer().CN
+				if certLoad.get_issuer().CN != None:
+					certinfo["deliver"] = certLoad.get_issuer().CN
+				elif certLoad.get_issuer().O != None:
+					certinfo["deliver"] = certLoad.get_issuer().O
+				else:
+					certinfo["deliver"] = "Introuvable"
 				return certinfo
 			except ssl.SSLError as err:
 				#print str(err)
